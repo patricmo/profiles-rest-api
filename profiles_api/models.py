be930bbff91b85b01c5982b5b79701cdde58ad09
Patrick Moreau <pckmoreau@gmail.com>
2021-09-09 16:54:19 +0200

    Added custom Profile model-manager, and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    """manager for user prpofiles"""
+
+    def create_user(self, email, name, password=None):
+        """create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        # convert/encrypt password to hash
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        """create a new superuser profile"""
+        user = self.create_user(email, name, password)
+        user.is_superuser = True # from PermissionsMixin
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the systemctl"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """retreive full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """retreive short name of user"""
+        return self.name
+
+    def __str__(str):
+        """return default string of user"""
+        return self.email
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -124,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# overwrite user model to use
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
